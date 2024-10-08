4a366531d7b983c7d249dbef56651a30d75674f3
shivas1432 <shivashanker7337@gmail.com>
2024-10-08 10:48:25 +0100

    Fix cross-browser compatibility

---
+++ src/utils/utility468.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility468 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format468 = (value) => {
+  return value.toString().toLowerCase();
+};
