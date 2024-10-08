13b8425cd4497b3a302bbd91a98cc950cefaec50
shivas1432 <shivashanker7337@gmail.com>
2024-10-08 12:42:51 +0100

    Implement wishlist feature

---
+++ src/utils/utility778.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility778 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format778 = (value) => {
+  return value.toString().toLowerCase();
+};
