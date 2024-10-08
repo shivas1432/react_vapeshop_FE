fdae37c07b8a4de885fd59151681ad6d0627f71b
shivas1432 <shivashanker7337@gmail.com>
2024-10-08 09:38:59 +0100

    Update component styling

---
+++ src/utils/utility743.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility743 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format743 = (value) => {
+  return value.toString().toLowerCase();
+};
