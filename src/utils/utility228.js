441c6a925c96ea54e66f2e6168782d1d082c828c
shivas1432 <shivashanker7337@gmail.com>
2024-10-09 17:41:29 +0100

    Add shopping cart features

---
+++ src/utils/utility228.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility228 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format228 = (value) => {
+  return value.toString().toLowerCase();
+};
