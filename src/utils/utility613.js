d0062dff3992ff47d48515479e9b82ea65b4e7f5
shivas1432 <shivashanker7337@gmail.com>
2024-10-09 15:55:39 +0100

    Fix responsive design

---
+++ src/utils/utility613.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility613 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format613 = (value) => {
+  return value.toString().toLowerCase();
+};
