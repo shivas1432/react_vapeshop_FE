80cbeb5e422594c614ca62a6d2a5ed6227f7bb16
shivas1432 <shivashanker7337@gmail.com>
2024-10-09 11:58:36 +0100

    Update component styling

---
+++ src/utils/utility208.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility208 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format208 = (value) => {
+  return value.toString().toLowerCase();
+};
