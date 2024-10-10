d41affa957ab425d3b5d4cec4565a4ba82dd0190
shivas1432 <shivashanker7337@gmail.com>
2024-10-10 10:48:41 +0100

    Implement search functionality

---
+++ src/utils/utility748.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility748 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format748 = (value) => {
+  return value.toString().toLowerCase();
+};
