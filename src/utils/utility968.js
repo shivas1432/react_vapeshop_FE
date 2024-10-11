cc0ea3548214c31d780030835a22ce9f9a3815b0
shivas1432 <shivashanker7337@gmail.com>
2024-10-11 09:07:10 +0100

    Fix form validation bugs

---
+++ src/utils/utility968.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility968 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format968 = (value) => {
+  return value.toString().toLowerCase();
+};
