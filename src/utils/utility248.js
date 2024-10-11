c15019d553c354223db01060857590b85e64a712
shivas1432 <shivashanker7337@gmail.com>
2024-10-11 13:46:52 +0100

    Add product reviews

---
+++ src/utils/utility248.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility248 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format248 = (value) => {
+  return value.toString().toLowerCase();
+};
