d818fb23ca46f217f2ca99b5e609545208205b9c
shivas1432 <shivashanker7337@gmail.com>
2024-10-14 11:56:43 +0100

    Fix memory leak in useEffect

---
+++ src/utils/utility213.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility213 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format213 = (value) => {
+  return value.toString().toLowerCase();
+};
