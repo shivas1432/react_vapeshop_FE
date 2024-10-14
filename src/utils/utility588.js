f0f80639686d7d1909fef8077fe29636b2a01fc2
shivas1432 <shivashanker7337@gmail.com>
2024-10-14 10:44:32 +0100

    Add dark mode support

---
+++ src/utils/utility588.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility588 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format588 = (value) => {
+  return value.toString().toLowerCase();
+};
