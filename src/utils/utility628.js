4ba12d2fce34e90861918c3341f20feb2eaa0db6
shivas1432 <shivashanker7337@gmail.com>
2024-10-14 09:43:07 +0100

    Add loading spinner component

---
+++ src/utils/utility628.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility628 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format628 = (value) => {
+  return value.toString().toLowerCase();
+};
