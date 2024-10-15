1e899be514757859fd4b5c8a014d98496057c0d6
shivas1432 <shivashanker7337@gmail.com>
2024-10-15 09:46:23 +0100

    Refactor component structure

---
+++ src/utils/utility568.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility568 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format568 = (value) => {
+  return value.toString().toLowerCase();
+};
