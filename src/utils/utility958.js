7ea5b0fde384a6317ad5d404826a782ccc6353bc
shivas1432 <shivashanker7337@gmail.com>
2024-10-16 16:23:10 +0100

    Improve error handling

---
+++ src/utils/utility958.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility958 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format958 = (value) => {
+  return value.toString().toLowerCase();
+};
