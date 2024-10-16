f044232e6f2151bf6c524aaf119115887fbff274
shivas1432 <shivashanker7337@gmail.com>
2024-10-16 16:52:31 +0100

    Add loading spinner component

---
+++ src/components/Component531.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Component531 = () => {
+  return (
+    <div className="component-531">
+      <h2>Component 531</h2>
+    </div>
+  );
+};
+
+export default Component531;
