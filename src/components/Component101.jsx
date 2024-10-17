7fc8772b80b704e74c2ccbd2f82a8d3afd05174a
shivas1432 <shivashanker7337@gmail.com>
2024-10-17 17:53:06 +0100

    Fix API endpoint configuration

---
+++ src/components/Component101.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Component101 = () => {
+  return (
+    <div className="component-101">
+      <h2>Component 101</h2>
+    </div>
+  );
+};
+
+export default Component101;
