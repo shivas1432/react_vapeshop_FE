60e4e1f5aa20a4f59c4386430e4a862e554712b7
shivas1432 <shivashanker7337@gmail.com>
2024-10-17 17:08:39 +0100

    Fix memory leak in useEffect

---
+++ src/components/Component121.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Component121 = () => {
+  return (
+    <div className="component-121">
+      <h2>Component 121</h2>
+    </div>
+  );
+};
+
+export default Component121;
