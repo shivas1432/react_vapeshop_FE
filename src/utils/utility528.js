7ee88b46602590cd2c09e614fdc160204dae2356
shivas1432 <shivashanker7337@gmail.com>
2024-10-17 09:59:08 +0100

    Update project dependencies

---
+++ src/utils/utility528.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility528 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format528 = (value) => {
+  return value.toString().toLowerCase();
+};
