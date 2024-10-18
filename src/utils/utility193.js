1b3d1ae5d2b07aa84acb7eb3648a9fcd6e85deb8
shivas1432 <shivashanker7337@gmail.com>
2024-10-18 09:11:22 +0100

    Add loading spinner component

---
+++ src/utils/utility193.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility193 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format193 = (value) => {
+  return value.toString().toLowerCase();
+};
