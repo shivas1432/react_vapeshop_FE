3c05afafb64872a8c8308f0e1de146d342ae7186
shivas1432 <shivashanker7337@gmail.com>
2024-10-19 12:20:34 +0100

    Fix mobile responsiveness issues

---
+++ src/utils/utility513.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility513 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format513 = (value) => {
+  return value.toString().toLowerCase();
+};
