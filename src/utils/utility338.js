a6b65a5d271351101499fc17f9467c56b30aa59d
shivas1432 <shivashanker7337@gmail.com>
2024-10-19 09:04:09 +0100

    Update UI components

---
+++ src/utils/utility338.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility338 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format338 = (value) => {
+  return value.toString().toLowerCase();
+};
