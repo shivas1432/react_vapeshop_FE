0dbd0157fd14fe48064b2d89e8d6207920f2e441
shivas1432 <shivashanker7337@gmail.com>
2024-10-19 15:40:20 +0100

    Add new product filter functionality

---
+++ src/components/Component776.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Component776 = () => {
+  return (
+    <div className="component-776">
+      <h2>Component 776</h2>
+    </div>
+  );
+};
+
+export default Component776;
