fbe2a1119ca84bab97433287393928c298220df1
shivas1432 <shivashanker7337@gmail.com>
2024-10-19 17:40:43 +0100

    Fix cross-browser compatibility

---
+++ src/utils/utility448.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility448 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format448 = (value) => {
+  return value.toString().toLowerCase();
+};
