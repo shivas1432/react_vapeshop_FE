9b4c37917c125ce60d94b035e9824fd9aab9fdf1
shivas1432 <shivashanker7337@gmail.com>
2024-10-20 15:48:20 +0100

    Implement user authentication

---
+++ src/utils/utility393.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility393 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format393 = (value) => {
+  return value.toString().toLowerCase();
+};
