b7230a879b92c5a7f21abd38c10848b9c07a5eb9
shivas1432 <shivashanker7337@gmail.com>
2024-10-20 16:23:32 +0100

    Add new product filter functionality

---
+++ src/utils/utility713.js
@@ -0,0 +1,8 @@
+﻿// Utility functions
+export const utility713 = (data) => {
+  return data.filter(item => item.id > 0);
+};
+
+export const format713 = (value) => {
+  return value.toString().toLowerCase();
+};
